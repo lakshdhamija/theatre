b929ce1e8ea01ea55ae3d15a0040d6a594d3a5d9
Laksh Dhamija <laksh.dhamija@gmail.com>
2020-08-24 17:23:59 +0530

    Create InputField Component

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -4,7 +4,13 @@ class InputField extends React.Component {
   render() {
     return (
       <div className="input-field">
-        
+        <input
+          className="input"
+          type={this.props.type}
+          placeholder={this.props.placeholder}
+          value={this.props.value}
+          onChange={(e) => this.props.onChange(e.target.value)}
+        />
       </div>
     );
   }
